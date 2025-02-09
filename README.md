77e8e42ddcca6a24fd675699dff1907fa6036a2c
christopher-besch <mail@chris-besch.com>
2025-02-09 19:54:27 +0100

    fix: don't overwrite input

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Selina's Homepage
+
+```bash
+rm -r ./portfolio_pages_out/
+mkdir ./portfolio_pages_out/
+find ./portfolio_pages/ -type f -printf "./fonts_loader/load_fonts_in_css.sh '%p' './portfolio_pages_out/%f'\n" | sh
+```
--- fonts_loader/load_fonts_in_css.sh
+++ fonts_loader/load_fonts_in_css.sh
@@ -14,6 +14,5 @@ echo "converting $1 to $2"
 sed "/OHtFGivqhAswi/e cat $FONTS_CSS_FILE" "$FONTS_CSS_TEMPLATE" | sed 's/OHtFGivqhAswi//' > "$FINAL_TEXT_TO_INCLUDE_IN_SVG"
 
 # Now we can load this template into the actual svg.
-# cp because we might be reading and writing from the same file.
 # cI0WWZKD2UKEj is the string to replace later on
-cp <(sed -E 's/(<svg [^>]+>)/\1\ncI0WWZKD2UKEj\n/' "$1" | sed "/cI0WWZKD2UKEj/e cat $FINAL_TEXT_TO_INCLUDE_IN_SVG" | sed 's/cI0WWZKD2UKEj//') "$2"
+sed -E 's/(<svg [^>]+>)/\1\ncI0WWZKD2UKEj\n/' "$1" | sed "/cI0WWZKD2UKEj/e cat $FINAL_TEXT_TO_INCLUDE_IN_SVG" | sed 's/cI0WWZKD2UKEj//' > "$2"
